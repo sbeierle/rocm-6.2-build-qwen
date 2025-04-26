f43529164c8dcbca7805a950efd4f29f53528f3d
ki-horscht <136800337+sbeierle@users.noreply.github.com>
2025-04-26 22:16:50 +0200

    venv_setup.sh

--- gfx1100_build/venv_setup.sh
+++ gfx1100_build/venv_setup.sh
@@ -6,3 +6,9 @@ pip install --upgrade pip
 pip install "numpy<2.0.0"
 pip install torch torchvision --index-url https://download.pytorch.org/whl/rocm6.1
 pip install transformers safetensors accelerate
+
+export ROCM_HOME=/opt/rocm
+export HIP_PATH=/opt/rocm
+export PATH=$ROCM_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$ROCM_HOME/lib:$ROCM_HOME/lib64:$LD_LIBRARY_PATH
+export HSA_OVERRIDE_GFX_VERSION=11.0.0
